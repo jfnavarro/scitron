a01b84239ef9ec9074b185ab2613003b9cdd5927
jfnavarro <jc.fernandez.navarro@gmail.com>
2021-02-12 18:02:47 +0100

    Updates

--- sarek_merge/parse_variants.py
+++ sarek_merge/parse_variants.py
@@ -28,95 +28,95 @@ def main(FILTER_DP,
     print('Loaded {} samples'.format(metasamples.shape[0]))
     print('Searching for variants in {}'.format(PATH))
 
-    # # FIRST parse VCF files and create a dictionary by variant key containing the variants info
-    # variants_dict = defaultdict(list)
-    # for index, row in metasamples.iterrows():
-    #     sample_id = row['SAMPLE_ID']
-    #     sample_patient = row['PATIENT_ID']
-    #     tumor_only = all(metasamples[metasamples['PATIENT_ID'] == sample_patient]['TUMOR'] == 1)
-    #     germline = all(metasamples[metasamples['PATIENT_ID'] == sample_patient]['TUMOR'] == 0)
-    #     # Retrieve variants
-    #     if tumor_only or germline:
-    #         # TODO make this a single reg exp
-    #         for elem in chain(Path(PATH).rglob('*{}_snpEff.ann.vcf.gz'.format(sample_id)),
-    #                           Path(PATH).rglob('*{}_variants_snpEff.ann.vcf.gz'.format(sample_id))):
-    #             variants_list = list()
-    #             if 'Strelka' in str(elem):
-    #                 print('Parsing variant in {}'.format(str(elem)))
-    #                 variants_list = parse_strelka_germline_variants(elem, FILTER_DP, FILTER_DP_PDX,
-    #                                                                 FILTER_VAF, DISABLE_EFFECT_FILTER)
-    #             if 'HaplotypeCaller' in str(elem) and '.g_snpEff' not in str(elem):
-    #                 print('Parsing variant in {}'.format(str(elem)))
-    #                 variants_list = parse_haplotypecaller_germline_variants(elem, FILTER_DP, FILTER_DP_PDX,
-    #                                                                         FILTER_VAF, DISABLE_EFFECT_FILTER)
-    #             if len(variants_list) > 0:
-    #                 for v in variants_list:
-    #                     v.sample = sample_id
-    #                     v.patient = sample_patient
-    #                     variants_dict[v.key].append(v)
-    #     else:
-    #         gdna = metasamples[(metasamples['PATIENT_ID'] == sample_patient)
-    #                            & (metasamples['TUMOR'] == 0)]['SAMPLE_ID'].to_numpy()[0]
-    #         #gdna = gdna[:gdna.rfind('_')]
-    #         if row['TUMOR'] != 0:
-    #             # TODO make this a single reg exp
-    #             for elem in chain(Path(PATH).rglob('*{}_vs_{}_snpEff.ann.vcf.gz'.format(sample_id, gdna)),
-    #                               Path(PATH).rglob('*{}_vs_{}_variants_snpEff.ann.vcf.gz'.format(sample_id, gdna))):
-    #                 variants_list = list()
-    #                 if 'StrelkaBP' in str(elem) and 'somatic_snvs' in str(elem):
-    #                     print('Parsing variant in {}'.format(str(elem)))
-    #                     variants_list = parse_strelka_variants(elem, FILTER_DP, FILTER_DP_PDX,
-    #                                                            FILTER_VAF, DISABLE_EFFECT_FILTER)
-    #                 if 'StrelkaBP' in str(elem) and 'somatic_indels' in str(elem):
-    #                     print('Parsing variant in {}'.format(str(elem)))
-    #                     variants_list = parse_strelka_indel_variants(elem, FILTER_DP, FILTER_DP_PDX,
-    #                                                                  FILTER_VAF, DISABLE_EFFECT_FILTER)
-    #                 if 'Mutect2_filtered' in str(elem):
-    #                     print('Parsing variant in {}'.format(str(elem)))
-    #                     variants_list = parse_mutect_variants(elem, FILTER_DP, FILTER_DP_PDX,
-    #                                                           FILTER_VAF, DISABLE_EFFECT_FILTER)
-    #                 if len(variants_list) > 0:
-    #                     for v in variants_list:
-    #                         v.sample = sample_id
-    #                         v.patient = sample_patient
-    #                         variants_dict[v.key].append(v)
-    #
-    # # SECOND parse the dictionary to create a table with the variants and useful information
-    # ALL_SAMPLES = list(metasamples['SAMPLE_ID'])
-    # ALL_PATIENTS = list(metasamples['PATIENT_ID'])
-    # with open('merged_variants.txt', 'w') as handler:
-    #     with open('tmp_anno.txt', 'w') as handler2:
-    #         header_samples = '\t'.join(['{}-{}'.format(x, y) for x, y in zip(ALL_SAMPLES, ALL_PATIENTS)])
-    #         handler.write(
-    #             'VARIANT_KEY\tGENE\tEFFECT\tAACHANGE\tTYPE\tNUM_SAMPLES\tNUM_PATIENTS\t{}\n'.format(header_samples))
-    #         for key, items in variants_dict.items():
-    #             samples_variants_filtered = defaultdict(list)
-    #             effects = set()
-    #             patients = set()
-    #             for v in items:
-    #                 effects.update(v.effects)
-    #                 patients.add(v.patient)
-    #                 samples_variants_filtered[v.sample].append('{}:{};{};{}'.format(v.caller, v.DP, v.AD, v.VAF))
-    #             if len(samples_variants_filtered) >= 1:
-    #                 key_split = key.split(':')
-    #                 chr = key_split[0]
-    #                 pos = key_split[1].split()[0]
-    #                 ref = key_split[1].split()[1].split('>')[0]
-    #                 alt = key_split[1].split()[1].split('>')[1]
-    #                 num_patients = len(patients)
-    #                 num_samples = len(samples_variants_filtered)
-    #                 samples_str = '\t'.join(['|'.join(set(samples_variants_filtered[s]))
-    #                                          if s in samples_variants_filtered else 'Na' for s in ALL_SAMPLES])
-    #                 to_write = '{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(key,
-    #                                                                      '|'.join(set([x[1] for x in effects])),
-    #                                                                      '|'.join(set([x[0] for x in effects])),
-    #                                                                      '|'.join(set([x[2] for x in effects])),
-    #                                                                      '|'.join(set([x[3] for x in effects])),
-    #                                                                      num_samples,
-    #                                                                      num_patients,
-    #                                                                      samples_str)
-    #                 handler.write(to_write)
-    #                 handler2.write('{}\t{}\t{}\t{}\t{}\n'.format(chr, pos, pos, ref, alt))
+    # FIRST parse VCF files and create a dictionary by variant key containing the variants info
+    variants_dict = defaultdict(list)
+    for index, row in metasamples.iterrows():
+        sample_id = row['SAMPLE_ID']
+        sample_patient = row['PATIENT_ID']
+        tumor_only = all(metasamples[metasamples['PATIENT_ID'] == sample_patient]['TUMOR'] == 1)
+        germline = all(metasamples[metasamples['PATIENT_ID'] == sample_patient]['TUMOR'] == 0)
+        # Retrieve variants
+        if tumor_only or germline:
+            # TODO make this a single reg exp
+            for elem in chain(Path(PATH).rglob('*{}_snpEff.ann.vcf.gz'.format(sample_id)),
+                              Path(PATH).rglob('*{}_variants_snpEff.ann.vcf.gz'.format(sample_id))):
+                variants_list = list()
+                if 'Strelka' in str(elem):
+                    print('Parsing variant in {}'.format(str(elem)))
+                    variants_list = parse_strelka_germline_variants(elem, FILTER_DP, FILTER_DP_PDX,
+                                                                    FILTER_VAF, DISABLE_EFFECT_FILTER)
+                if 'HaplotypeCaller' in str(elem) and '.g_snpEff' not in str(elem):
+                    print('Parsing variant in {}'.format(str(elem)))
+                    variants_list = parse_haplotypecaller_germline_variants(elem, FILTER_DP, FILTER_DP_PDX,
+                                                                            FILTER_VAF, DISABLE_EFFECT_FILTER)
+                if len(variants_list) > 0:
+                    for v in variants_list:
+                        v.sample = sample_id
+                        v.patient = sample_patient
+                        variants_dict[v.key].append(v)
+        else:
+            gdna = metasamples[(metasamples['PATIENT_ID'] == sample_patient)
+                               & (metasamples['TUMOR'] == 0)]['SAMPLE_ID'].to_numpy()[0]
+            #gdna = gdna[:gdna.rfind('_')]
+            if row['TUMOR'] != 0:
+                # TODO make this a single reg exp
+                for elem in chain(Path(PATH).rglob('*{}_vs_{}_snpEff.ann.vcf.gz'.format(sample_id, gdna)),
+                                  Path(PATH).rglob('*{}_vs_{}_variants_snpEff.ann.vcf.gz'.format(sample_id, gdna))):
+                    variants_list = list()
+                    if 'StrelkaBP' in str(elem) and 'somatic_snvs' in str(elem):
+                        print('Parsing variant in {}'.format(str(elem)))
+                        variants_list = parse_strelka_variants(elem, FILTER_DP, FILTER_DP_PDX,
+                                                               FILTER_VAF, DISABLE_EFFECT_FILTER)
+                    if 'StrelkaBP' in str(elem) and 'somatic_indels' in str(elem):
+                        print('Parsing variant in {}'.format(str(elem)))
+                        variants_list = parse_strelka_indel_variants(elem, FILTER_DP, FILTER_DP_PDX,
+                                                                     FILTER_VAF, DISABLE_EFFECT_FILTER)
+                    if 'Mutect2_filtered' in str(elem):
+                        print('Parsing variant in {}'.format(str(elem)))
+                        variants_list = parse_mutect_variants(elem, FILTER_DP, FILTER_DP_PDX,
+                                                              FILTER_VAF, DISABLE_EFFECT_FILTER)
+                    if len(variants_list) > 0:
+                        for v in variants_list:
+                            v.sample = sample_id
+                            v.patient = sample_patient
+                            variants_dict[v.key].append(v)
+
+    # SECOND parse the dictionary to create a table with the variants and useful information
+    ALL_SAMPLES = list(metasamples['SAMPLE_ID'])
+    ALL_PATIENTS = list(metasamples['PATIENT_ID'])
+    with open('merged_variants.txt', 'w') as handler:
+        with open('tmp_anno.txt', 'w') as handler2:
+            header_samples = '\t'.join(['{}-{}'.format(x, y) for x, y in zip(ALL_SAMPLES, ALL_PATIENTS)])
+            handler.write(
+                'VARIANT_KEY\tGENE\tEFFECT\tAACHANGE\tTYPE\tNUM_SAMPLES\tNUM_PATIENTS\t{}\n'.format(header_samples))
+            for key, items in variants_dict.items():
+                samples_variants_filtered = defaultdict(list)
+                effects = set()
+                patients = set()
+                for v in items:
+                    effects.update(v.effects)
+                    patients.add(v.patient)
+                    samples_variants_filtered[v.sample].append('{}:{};{};{}'.format(v.caller, v.DP, v.AD, v.VAF))
+                if len(samples_variants_filtered) >= 1:
+                    key_split = key.split(':')
+                    chr = key_split[0]
+                    pos = key_split[1].split()[0]
+                    ref = key_split[1].split()[1].split('>')[0]
+                    alt = key_split[1].split()[1].split('>')[1]
+                    num_patients = len(patients)
+                    num_samples = len(samples_variants_filtered)
+                    samples_str = '\t'.join(['|'.join(set(samples_variants_filtered[s]))
+                                             if s in samples_variants_filtered else 'Na' for s in ALL_SAMPLES])
+                    to_write = '{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(key,
+                                                                         '|'.join(set([x[1] for x in effects])),
+                                                                         '|'.join(set([x[0] for x in effects])),
+                                                                         '|'.join(set([x[2] for x in effects])),
+                                                                         '|'.join(set([x[3] for x in effects])),
+                                                                         num_samples,
+                                                                         num_patients,
+                                                                         samples_str)
+                    handler.write(to_write)
+                    handler2.write('{}\t{}\t{}\t{}\t{}\n'.format(chr, pos, pos, ref, alt))
 
     # Annotate with the annovar the variants
     ANNOVAR_PATH = os.environ['ANNOVAR_PATH']
